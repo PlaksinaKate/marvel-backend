f7ed336fa9d22d0655ec5327b4f7bbc66c914863
PlaksinaKate <plaksina.larisa@yandex.ru>
2024-04-25 00:54:31 +0700

    feat: add master database

--- database/index.js
+++ database/index.js
@@ -1,6 +1,8 @@
 import { clients } from "./client.js";
+import { masters } from "./master.js";
 
 export const database = {
   clients,
+  masters
 }
 
---
+++ database/master.js
@@ -0,0 +1,28 @@
+import { pool } from "./pool.js";
+
+async function getMasters() {
+  const [rows] = await pool.query("SELECT * FROM master");
+  return rows;
+}
+
+async function getMaster(id) {
+  const [rows] = await pool.query(`SELECT * FROM master WHERE id_master = ?`, [
+    id,
+  ]);
+  return rows[0];
+}
+
+async function createMaster(name, position, description) {
+  const [result] = await pool.query(
+    `INSERT INTO master (name, position, description) VALUES (?, ?)`,
+    [name, position, description]
+  );
+
+  return result.insertId;
+}
+
+export const masters = {
+  getMasters,
+  getMaster,
+  createMaster,
+};
