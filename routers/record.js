85301b468bcd9af98fd8273444f7b679db6dc271
PlaksinaKate <plaksina.larisa@yandex.ru>
2024-05-15 16:41:37 +0700

    fix: records response

--- routers/record.js
+++ routers/record.js
@@ -5,38 +5,54 @@ export const record = express.Router();
 
 record.get("", async (req, res) => {
   const { client, master } = req.query;
-  let record;
+  let records;
 
   if (client) {
-    record = await database.records.getRecordByClientId(client);
+    records = await database.records.getRecordByClientId(client);
   } else if (master) {
-    record = await database.records.getRecordByMasterId(master);
+    records = await database.records.getRecordByMasterId(master);
   } else {
-    record = await database.records.getRecords();
+    records = await database.records.getRecords();
   }
 
-  res.send(record);
+  res.send({
+    status: "ok",
+    records
+  });
 });
 
 record.get("/:id", async (req, res) => {
   const id = req.params.id;
-  const record = await database.records.getRecord(id);
-  res.send(record);
+  const records = await database.records.getRecord(id);
+  res.send({
+    status: "ok",
+    records,
+  });
 });
 
 record.post("", async (req, res) => {
   const { id_service, id_master, id_client, data_time } = req.body;
-  const record = await database.records.createRecord(
+  const records = await database.records.createRecord(
     id_service,
     id_master,
     id_client,
     data_time
   );
-  res.status(201).send(record);
+  res.status(201).send({
+    status: "ok",
+    records
+  });
 });
 
 record.put("", async (req, res) => {
   const { id, id_master, date_time } = req.body;
-  const newClientId = await database.records.updateRecord(id, id_master, date_time);
-  res.status(200).send(newClientId);
+  const newClientId = await database.records.updateRecord(
+    id,
+    id_master,
+    date_time
+  );
+  res.status(200).send({
+    status: "ok",
+    newClientId,
+  });
 });
